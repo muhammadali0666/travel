6454b90e2bc19ce4a6de0b1a2954d4bdbf1692db
muhammadali0666 <muhammadalishuhratjonov50@gmail.com>
2022-05-22 10:38:40 +0500

    init

---
+++ js/index.js
@@ -0,0 +1,13 @@
+const cash = 9642000;
+const money = prompt("Summani kriting") * 1;
+
+
+if (money >= 9642000) {
+  console.log("Oq yo’l, Alisher!");
+} else {
+  console.log(` ${cash - money} so'm yetmadi,
+    Alisher ozgina sabr qilish kerak bo’lar ekan.`);
+}
+
+
+ 
